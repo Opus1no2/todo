8a38a3e1c7f982f98ddfb51a9cd023d7e8edea06
Opus1no2 <tillotson.travis@gmail.com>
2021-07-19 08:40:15 -0500

    add production url based on node_env

--- frontend/src/api/interceptor.js
+++ frontend/src/api/interceptor.js
@@ -1,7 +1,13 @@
 import axios from 'axios'
 import useToken from '../hooks/useToken'
 
-axios.defaults.baseURL = 'http://127.0.0.1:5000/api'
+let endpoint = 'http://127.0.0.1:5000/api'
+
+if (process.env.NODE_ENV === 'production') {
+  endpoint = 'http://todo-prod-blue.eba-n2wwiv67.us-west-2.elasticbeanstalk.com/api'
+}
+
+axios.defaults.baseURL = endpoint
 
 axios.interceptors.request.use((config) => {
   const token = useToken().get()
